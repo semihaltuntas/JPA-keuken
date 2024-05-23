5bf9fede9d11b3186ed86efc4ff42ca60bb41b73
semihkenan <altuntas.semih34@gmail.com>
2024-05-23 16:12:35 +0200

    1.dag JPA-Hibernate

--- src/main/java/be/vdab/keuken/KeukenApplication.java
+++ src/main/java/be/vdab/keuken/KeukenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KeukenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KeukenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KeukenApplication.class, args);
+    }
 
 }
